7dddcb462e9f7fba9d2a5ddf66a370f75e43695f
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-03-02 16:03:47 -0500

    routing implemented

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import Navigation from '../Navigation'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import Navigation from '../Navigation';
+import Landing from '../Landing'
+import SignUp from '../SignUp'
+import SignIn from '../SignIn'
+import PasswordForget from '../PasswordForget'
+import Home from '../Home'
+import Account from '../Account'
+import Admin from '../Admin'
 
-
+import * as Routes from '../../constants/routes'
 
 const App = () => ( 
+    <Router>
     <div>
+      <div>
         <h1>App</h1>
+      </div>
         <Navigation  />
+        <hr />
+
+        <Route exact path={Routes.LANDING} component={Landing} />
+        <Route path={Routes.SIGN_UP} component={SignUp} />
+        <Route path={Routes.SIGN_IN} component={SignIn} />
+        <Route path={Routes.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={Routes.HOME} component={Home} />
+        <Route path={Routes.ACCOUNT} component={Account} />
+        <Route path={Routes.ADMIN} component={Admin} />
     </div>
+    </Router>
  );
 
  
